60fa430cb658cf8b981a2cf4c323d7b8887e413c
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-09 21:45:21 +0600

    basic visited flags added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,47 +1,63 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
-import './Countries.css'
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
-    const [visitedCountries, setVisitedCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    const handleVisitedCountry = country =>{
-        console.log('add this to your visited country');
-        const newVisitedCountries = [...visitedCountries, country];
-        setVisitedCountries(newVisitedCountries);
-    }
-    return (
-        <div>
-            <h3>Countries: {countries.length}</h3>
-            <div>
-                <h5>Visited countries: {visitedCountries.length} </h5>
-                <ul>
-                    {
-                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
-                    }
-                </ul>
-            </div>
-           <div className="country-container">
-           {
-                countries.map(country => <Country 
-                    key={country.cca3} 
-                    country={country}
-                    handleVisitedCountry={handleVisitedCountry}
-                    >
+  const handleVisitedCountry = (country) => {
+    console.log("add this to your visited country");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
 
-                    </Country>)
+  const handleVisitedFlags = (flag) => {
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  };
+
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      {/* visited countries  */}
+      <div>
+        <h5>Visited countries: {visitedCountries.length} </h5>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* visited countries flags  */}
+      <div className="flag-container">
+            {
+                visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
             }
-           </div>
-        </div>
-    );
+      </div>
+
+      {/* display countries */}
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlags={handleVisitedFlags}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
