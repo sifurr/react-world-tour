b1991658ad6494bee6459b1458a2cc83a26adeac
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-09 20:22:28 +0600

    visited country introduced

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,17 +5,36 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+
+    const handleVisitedCountry = country =>{
+        console.log('add this to your visited country');
+        console.log(country);
+    }
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited countries</h5>
+                <ul>
+
+                </ul>
+            </div>
            <div className="country-container">
            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                countries.map(country => <Country 
+                    key={country.cca3} 
+                    country={country}
+                    handleVisitedCountry={handleVisitedCountry}
+                    >
+
+                    </Country>)
             }
            </div>
         </div>
